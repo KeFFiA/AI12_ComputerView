09d817367c7ba8d2b96601df6bde12f8ccced417
KeFFiA <ogienko.12003@gmail.com>
2025-07-16 12:22:52 +0300

    Add LLM Llama-3

--- LLM_Server/entrypoint.sh
+++ LLM_Server/entrypoint.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-MODEL_PATH="${MODEL_PATH:-/models/Meta-Llama-3-8B-Instruct.fp16.gguf}"
-HOST="${HOST:-0.0.0.0}"
-PORT="${PORT:-8000}"
-N_CTX="${N_CTX:-4096}"
-N_GPU_LAYERS="${N_GPU_LAYERS:-100}"
+set -e
 
-echo "🚀 Start llama-server:"
-echo "  MODEL_PATH:     $MODEL_PATH"
-echo "  HOST:           $HOST"
-echo "  PORT:           $PORT"
-echo "  N_CTX:          $N_CTX"
-echo "  N_GPU_LAYERS:   $N_GPU_LAYERS"
+MODEL_PATH="${MODEL_PATH:-/models/Meta-Llama-3-8B-Instruct.fp16.gguf}"
+PORT="${PORT:-8000}"
+HOST="${HOST:-0.0.0.0}"
+N_GPU_LAYERS="${N_GPU_LAYERS:-100}"
+N_CTX="${N_CTX:-4096}"
+
+echo "Starting llama-server:"
+echo "  Модель: $MODEL_PATH"
+echo "  Host: $HOST"
+echo "  Port: $PORT"
+echo "  GPU layers: $N_GPU_LAYERS"
+echo "  Context size: $N_CTX"
 
 exec /llama.cpp/build/bin/llama-server \
     --model "$MODEL_PATH" \
