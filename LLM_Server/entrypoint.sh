b617ff1fb69d972450c9d2a1efcefcb91b90575e
KeFFiA <ogienko.12003@gmail.com>
2025-07-16 14:58:30 +0300

    Add LLM Llama-3

--- LLM_Server/entrypoint.sh
+++ LLM_Server/entrypoint.sh
@@ -5,8 +5,13 @@ set -e
 MODEL_PATH="${MODEL_PATH:-/models/Meta-Llama-3-8B-Instruct.fp16.gguf}"
 PORT="${PORT:-8000}"
 HOST="${HOST:-0.0.0.0}"
-N_GPU_LAYERS="${N_GPU_LAYERS:-100}"
-N_CTX="${N_CTX:-4096}"
+N_GPU_LAYERS="${N_GPU_LAYERS:-999}"
+N_CTX="${N_CTX:-8192}"
+THREADS="${THREADS:-16}"
+THREADS_BATCH="${THREADS_BATCH:-8}"
+BATCH_SIZE="${BATCH_SIZE:-1024}"
+UBATCH_SIZE="${UBATCH_SIZE:-1024}"
+
 
 echo "== STARTING LLAMA SERVER ==" >&1
 echo "  Модель: $MODEL_PATH" >&1
@@ -23,6 +28,12 @@ ls -la /models >&1
 exec /llama.cpp/build/bin/llama-server \
     --model "$MODEL_PATH" \
     --gpu-layers "$N_GPU_LAYERS" \
+    --threads "$THREADS" \
+    --threads-batch "$THREADS_BATCH" \
+    --flash-attn \
+    --mlock \
+    --batch-size "$BATCH_SIZE"\
+    --ubatch-size "$UBATCH_SIZE"\
     --ctx-size "$N_CTX" \
     --host "$HOST" \
     --port "$PORT"
