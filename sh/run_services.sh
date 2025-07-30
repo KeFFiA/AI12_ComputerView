72fc0f42d9b0251cd6956ff1f5c15be10ebe989a
KeFFiA <ogienko.12003@gmail.com>
2025-07-31 00:28:07 +0300

    Add claims parser

--- sh/run_services.sh
+++ sh/run_services.sh
@@ -6,12 +6,12 @@ echo "Starting all services..."
 echo " "
 
 echo "Starting medical_parser..."
-bash "$BASE_DIR/run_medical_parser.sh"
+bash "$BASE_DIR/sh/run_medical_parser.sh"
 
 echo "Starting claims_parser..."
-bash "$BASE_DIR/run_claims_parser.sh"
+bash "$BASE_DIR/sh/run_claims_parser.sh"
 
 echo "Starting llama3-api..."
-bash "$BASE_DIR/run_llama3.sh"
+bash "$BASE_DIR/sh/run_llama3.sh"
 
 echo "All services started."
