9c2ce5b68818dd44e10f7ec0453c5283b85fc334
KeFFiA <ogienko.12003@gmail.com>
2025-08-19 17:31:55 +0300

    Add ollama for tests

--- test/chains/enrich_chain.py
+++
@@ -1,6 +0,0 @@
-def enrich_cause(data):
-    engine = data.get("aircraft", {}).get("engine")
-    if engine and "cause" in data:
-        data["cause"] += f"\nEngine Info: {engine}"
-    return data
-
